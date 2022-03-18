ba6896e36fc69f203e9a696d4f5e31f69ec798c3
Marcio Roseno da Silva <100876565+Marcioroseno@users.noreply.github.com>
2022-03-18 18:31:46 -0300

    corrigindo as bordas dos botões

---
+++ U1_exercicio 3/cadepessoas_marcio/cadepessoas_marcio/src/test/java/br/com/marcio/cadpessoas/CadepessoasMarcioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcio.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadepessoasMarcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
