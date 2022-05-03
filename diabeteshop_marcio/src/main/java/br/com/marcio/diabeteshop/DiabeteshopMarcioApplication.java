e3616f4f3b5340fc701c843edd9adf170cc056af
Marcio Roseno da Silva <100876565+Marcioroseno@users.noreply.github.com>
2022-05-03 16:47:17 -0300

    Organizando o projeto

---
+++ diabeteshop_marcio/src/main/java/br/com/marcio/diabeteshop/DiabeteshopMarcioApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcio.diabeteshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeteshopMarcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiabeteshopMarcioApplication.class, args);
+	}
+
+}
---
+++ diabeteshop_marcio/src/test/java/br/com/marcio/diabeteshop/DiabeteshopMarcioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcio.diabeteshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeteshopMarcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
